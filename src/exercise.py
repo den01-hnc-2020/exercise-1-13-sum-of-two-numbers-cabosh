669395054d790161a4bae184a8f0164877a7b9a8
cabosh <callum.bosher@gmail.com>
2020-05-15 12:09:11 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    print("Addition Calculator")
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+
+    print("The sum of your 2 numbers is " + str(num1 + num2))
 
 if __name__ == '__main__':
     main()
